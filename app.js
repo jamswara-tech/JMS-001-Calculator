e67d1b4eaca2adeb5394108e2b17751237986563
Shubo <shubodey56@gmail.com>
2024-09-25 12:18:45 +0100

    sum to subtraction

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ const express = require('express');
 //creating an instance of an express app by calling express()
 const app = express();
 
-app.get('/', (req, res) => {
-    // Respond with a simple HTML form or result of an addition
-    res.send(`
-        <form action="/add" method="GET">
-            <label>First Number: <input type="number" name="num1"></label><br>
-            <label>Second Number: <input type="number" name="num2"></label><br>
-            <button type="submit">Add</button>
-        </form>
-    `);
-});
-app.get('/add', (req, res) => {
-    const num1 = parseFloat(req.query.num1);
-    const num2 = parseFloat(req.query.num2);
-    const sum = num1 + num2;
-    res.send(`The sum of ${num1} and ${num2} is ${sum}`);
-});
+// app.get('/', (req, res) => {
+//     // Respond with a simple HTML form or result of an addition
+//     res.send(`
+//         <form action="/add" method="GET">
+//             <label>First Number: <input type="number" name="num1"></label><br>
+//             <label>Second Number: <input type="number" name="num2"></label><br>
+//             <button type="submit">Add</button>
+//         </form>
+//     `);
+// });
+// app.get('/add', (req, res) => {
+//     const num1 = parseFloat(req.query.num1);
+//     const num2 = parseFloat(req.query.num2);
+//     const sum = num1 + num2;
+//     res.send(`The sum of ${num1} and ${num2} is ${sum}`);
+// });
 
 app.get('/', (req, res) => {
     // Respond with a simple HTML form or result of an addition
